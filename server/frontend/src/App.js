3c4b9296309a974ec4c20de1e9791d7566912a3c
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2024-11-12 19:35:35 -0700

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,10 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 
+urlpatterns = [
+    path('register/', TemplateView.as_view(template_name="index.html")),
+]
+
 function App() {
   return (
     <Routes>

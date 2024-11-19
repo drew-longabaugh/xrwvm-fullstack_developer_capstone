90138d624d26d174d0cd39cc54b7740a15535a89
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2024-11-18 20:50:30 -0700

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-    path('register/', views.registration, name='register'),
+    path(route='login', view=views.login_user, name='login'),
     
     # path for login
     path(route='login', view=views.login_user, name='login'),

810620684592eb1d7872cf4726d56aee2bdb8c66
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2024-11-22 22:21:28 -0700

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -74,13 +74,13 @@ TEMPLATES = [
             os.path.join(BASE_DIR, 'frontend/build/static'),
             ],
         'APP_DIRS': True,
-          'OPTIONS': {
+        'OPTIONS': {
               'context_processors': [
                   'django.template.context_processors.debug',
                   'django.template.context_processors.request',
                   'django.contrib.auth.context_processors.auth',
-                      'django.contrib.messages.context_processors.messages',
-            ],
+                  'django.contrib.messages.context_processors.messages',
+              ],
         },
     },
 ]

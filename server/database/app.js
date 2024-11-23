45ed2dc87fba88a04b99ea2ea542025479b0679d
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2024-11-22 21:09:12 -0700

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+/*jshint esversion: 8 */
 const mongoose = require('mongoose');
+/*jshint esversion: 8 */
 const fs = require('fs');
-const  cors = require('cors')
-const app = express()
+/*jshint esversion: 8 */
+const  cors = require('cors');
+/*jshint esversion: 8 */
+const app = express();
+/*jshint esversion: 8 */
 const port = 3030;
-
-app.use(cors())
+/*jshint esversion: 8 */
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
@@ -34,7 +39,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -91,8 +96,8 @@ app.get('/fetchDealer/:id', async (req, res) => {
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0]['id']+1
+  const documents = await Reviews.find().sort( { id: -1 } );
+  let new_id = documents[0]['id']+1;
 
   const review = new Reviews({
 		"id": new_id,

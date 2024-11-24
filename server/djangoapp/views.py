b5cc0025bc1252872190a6fbab2576edbc2dfe48
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2024-11-23 21:03:50 -0700

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -330,7 +330,7 @@ def add_review(request):
     try:
         response = post_review(data)
         return JsonResponse({"status" : 200})
-         except Exception as e:  # Catch other unexpected errors
+        except Exception as e:  # Catch other unexpected errors
             print(f"Error: {e}")
             return JsonResponse({"status" : 401, 
                                  "message" : "Error in posting review"})

375c42bafcb0a8effb3f07baa6d517598e8a286b
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2024-11-23 21:56:42 -0700

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -7,7 +7,7 @@ class CarMake(models.Model):
 
     name = models.CharField(max_length=20)  # Renamed from 'models'
     description = models.CharField
-    (max_length = 100)  # Increased length for better descriptions
+    max_length = 100  # Increased length for better descriptions
 
     def __str__(self):
         return f"{self.name} - {self.description}"

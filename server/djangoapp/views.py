9dc9db2a5979b9756733027d885dd171689c8f94
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2024-12-09 17:54:30 -0700

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from django.contrib.auth import authenticate, login
+from .restapis import get_request, analyze_review_sentiments, post_review
 import logging
 import json
 
@@ -160,18 +161,22 @@ def get_dealer_reviews(request, dealer_id):
         return JsonResponse({"status": 400, "message": "Bad Request"})
 
 
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
+
 def add_review(request):
-    """Submit a review."""
-    if request.user.is_authenticated:
+    if(request.user.is_anonymous == False):
         data = json.loads(request.body)
         try:
-            post_review(data)
-            return JsonResponse({"status": 200})
-        except Exception as e:
-            logger.error(f"Error in posting review: {e}")
-            return JsonResponse({
-                "status": 401,
-                "message": "Error in posting review"
-            })
+            response = post_review(data)
+            return JsonResponse({"status":200})
+        except:
+            return JsonResponse({"status":401,"message":"Error in posting review"})
     else:
-        return JsonResponse({"status": 403, "message": "Unauthorized"})
+        return JsonResponse({"status":403,"message":"Unauthorized"})

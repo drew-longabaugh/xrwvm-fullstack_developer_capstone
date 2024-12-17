8c5f83b10630a45cf59840c1799761ae94f864dc
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2024-12-16 19:36:51 -0700

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,7 +13,9 @@ urlpatterns = [
 
     # path for dealer reviews view
    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'), path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', 
+    view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
         path(route='get_dealers/<str:state>',
             view=views.get_dealerships, name='get_dealers_by_state'),
     path(route='dealer/<int:dealer_id>',

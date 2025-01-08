4339c180e866b9abc715823a252a1dc1b8fd7b6d
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2025-01-07 18:11:42 -0700

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,10 +30,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://drewlongabau-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://drewlongabau-8000.theiadockernext-0-labs-prod-theiak8s-
+    4-tor01.proxy.cognitiveclass.ai'
                 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://drewlongabau-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    'https://drewlongabau-8000.theiadockernext-0-labs-prod-theiak8s-
+    4-tor01.proxy.cognitiveclass.ai',
                        ]
 
 REST_FRAMEWORK = {
@@ -145,7 +147,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static'),
-    os.path.join(BASE_DIR, 'frontend/build'),
-    os.path.join(BASE_DIR, 'frontend/build/static'),
+    os.path.join(BASE_DIR ,'frontend/static'),
+    os.path.join(BASE_DIR , 'frontend/build'),
+    os.path.join(BASE_DIR , 'frontend/build/static'),
 ]
